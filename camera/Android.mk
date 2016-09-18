d72ae1ce72faae14cd2de1f2ea1c34027bd9455d
codeworkx <codeworkx@cyanogenmod.org>
2016-09-18 19:41:08 +0200

    camera: Add camera wrapper
    Change-Id: Ia1be3c2a22dfc5fbc66a6cbf2c40b94dfe145452

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 #
 # Copyright (C) 2017 The LineageOS Project
+=======
+# Copyright (C) 2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+>>>>>>> e5b2285 (camera: Add camera wrapper)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +19,39 @@
 # limitations under the License.
 #
 
+
 LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp \
+    Camera2Wrapper.cpp \
+    Camera3Wrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    libcutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_C_INCLUDES := \
+    framework/native/include \
+    frameworks/native/include/media/openmax \
+    system/media/camera/include
+
+LOCAL_32_BIT_ONLY := true
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
@@ -27,4 +64,5 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := true
+
 include $(BUILD_SHARED_LIBRARY)
