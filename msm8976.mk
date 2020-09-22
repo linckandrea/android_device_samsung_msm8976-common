c6b25ce00226e8954c03ebce913d7b0fdc0903ae
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    power: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- msm8976.mk
+++ msm8976.mk
@@ -256,7 +256,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
