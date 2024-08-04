934bb873054c5fd566e556b3320530a3e775d399
linckandrea <linckandrea01@gmail.com>
2024-08-04 23:34:26 +0200

    8976: copy aanc_tuning_mixer_wcd9330.txt
     * this should fix
    
    E MCS-RT-CTL: Can't open the configuration file /system/etc/aanc_tuning_mixer_wcd9330.txt.

--- msm8976.mk
+++ msm8976.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer_wcd9330.txt \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
