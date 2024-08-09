30880e6a32e7e76b80639be1084da9d73929363b
linckandrea <linckandrea01@gmail.com>
2024-08-09 18:30:13 +0200

    8976-common: Fixup:Create ramdisk symlinks & folders at build-time
     * missed in @8b0db50

--- config.fs
+++ config.fs
@@ -7,6 +7,36 @@ value:2951
 [AID_VENDOR_RFS_SHARED]
 value:2952
 
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
+caps: 0
+
+[efs/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_RADIO
+caps: 0
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware-modem/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
 [system/vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
