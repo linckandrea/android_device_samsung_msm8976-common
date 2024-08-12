aef5481722b8a574a4c537e3162439b8eb066013
linckandrea <linckandrea01@gmail.com>
2024-08-12 16:41:34 +0200

    remove eng tags
    Change-Id: I7d17edf8d1476f6f140a01d2c91a3801c952d2f5

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -8,5 +8,4 @@ LOCAL_C_INCLUDES := \
     bootable/recovery/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_samsung
-LOCAL_MODULE_TAGS := eng
 include $(BUILD_STATIC_LIBRARY)
