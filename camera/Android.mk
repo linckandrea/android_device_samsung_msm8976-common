7b1cdb429b1bc3eed2286653dcbca5afde2e00ed
linckandrea <linckandrea01@gmail.com>
2024-08-14 18:41:33 +0200

    camera: remove wrapper

--- camera/Android.mk
+++ camera/Android.mk
@@ -18,27 +18,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    framework/native/include \
-    system/media/camera/include
-
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils android.hidl.token@1.0-utils libgui libhidltransport
-
-LOCAL_STATIC_LIBRARIES := libarect
-
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_32_BIT_ONLY := true
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     CameraParameters.cpp \
     Fence.cpp
