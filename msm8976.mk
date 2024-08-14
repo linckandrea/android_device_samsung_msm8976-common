a4fca4889c04ceeddad6005aadc659af1f95497a
linckandrea <linckandrea01@gmail.com>
2024-08-14 23:08:43 +0200

    Revert "8976: camera back to passtrough"
    This reverts commit eae56f60cc4a239842f6874848419e1fb43cbbbc.

--- msm8976.mk
+++ msm8976.mk
@@ -115,6 +115,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8952 \
     libshims_camera
 
