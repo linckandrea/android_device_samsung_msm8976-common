ef364d04aadca837bf23f6350ade17105d88f7e4
linckandrea <linckandrea01@gmail.com>
2024-08-17 23:07:15 +0200

    8976: adjust for recent vendor changes  * move configs to vendor  * pin hex-edited blobs

--- msm8976.mk
+++ msm8976.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # TimeKeep
 PRODUCT_PACKAGES += \
