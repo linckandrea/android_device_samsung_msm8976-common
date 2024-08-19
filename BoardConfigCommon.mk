8a299f858056d0abd981635daa275c9aee20a122
linckandrea <linckandrea01@gmail.com>
2024-08-19 12:19:33 +0200

    Rild: make it use API 27

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,7 +115,8 @@ TARGET_BOARD_INFO_FILE := $(VENDOR_PATH)/board-info.txt
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /system/bin/cameraserver=24 \
-    /system/vendor/bin/mm-qcamera-daemon=24
+    /system/vendor/bin/mm-qcamera-daemon=24 \
+    /system/vendor/bin/hw/rild=27
 
 TARGET_USES_MEDIA_EXTENSIONS := true
 
