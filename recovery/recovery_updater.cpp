83db8dc62ac1f84a4067407d90ebced508bbb023
linckandrea <linckandrea01@gmail.com>
2024-09-20 13:18:13 +0200

    recovery: fixup compilation

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -26,6 +26,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <android-base/stringprintf.h>
+
 #include "edify/expr.h"
 #include "otautil/error_code.h"
 #include "updater/install.h"
@@ -160,6 +162,7 @@ err_ret:
 /* verify_trustzone("TZ_VERSION", "TZ_VERSION", ...) */
 Value * VerifyTrustZoneFn(const char *name, State *state,
                           const std::vector<std::unique_ptr<Expr>>& argv) {
+    auto updater = state->updater;
     char current_tz_version[TZ_VER_BUF_LEN];
     int ret;
 
@@ -175,8 +178,8 @@ Value * VerifyTrustZoneFn(const char *name, State *state,
     }
 
     for (auto& tz_version : args) {
-        uiPrintf(state, "Comparing TZ version %s to %s",
-                tz_version.c_str(), current_tz_version);
+        updater->UiPrint(android::base::StringPrintf("Comparing TZ version %s to %s",
+                tz_version.c_str(), current_tz_version));
         if (strncmp(tz_version.c_str(), current_tz_version, tz_version.length()) == 0) {
             return StringValue(strdup("1"));
         }
