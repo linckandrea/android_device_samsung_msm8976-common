5d1ee3b2d658abf36e0d5d05805a679e46cd3502
linckandrea <linckandrea01@gmail.com>
2024-09-23 09:55:42 +0200

    8976: add an init for recovery

--- msm8976.mk
+++ msm8976.mk
@@ -260,6 +260,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.power.rc \
     init.qcom.usb.rc \
+    init.recovery.qcom.rc \
     ueventd.qcom.rc
 
 # Recovery
--- recovery/Android.mk
+++ recovery/Android.mk
@@ -10,3 +10,11 @@ LOCAL_C_INCLUDES := \
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_samsung
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := root/init.recovery.qcom.rc
+LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
