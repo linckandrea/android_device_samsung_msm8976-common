479fcba5360bbfda3ac196fe81d00fee62437430
linckandrea <linckandrea01@gmail.com>
2024-09-25 15:32:33 +0200

    8976: build Snap
     * use HAL3 as open camera(that works)
    
    update Snap overlays

--- msm8976.mk
+++ msm8976.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8952 \
-    libshims_camera
+    libshims_camera \
+    Snap
 
 # DRM
 PRODUCT_PACKAGES += \
